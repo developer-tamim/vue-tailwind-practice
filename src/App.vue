2ffba485222c5a9f4bdadc8aa2977705a24e1b59
developer-tamim <tawsiftamim24@gmail.com>
2024-07-03 16:26:11 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="bg-white ">
+    <Navbar />
+    <HeroSection />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import HeroSection from './components/HeroSection.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    HeroSection,
+  },
+};
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
