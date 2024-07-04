0880db58a9021e72c5eed6c80b27c0e8f00feb73
developer-tamim <tawsiftamim24@gmail.com>
2024-07-04 23:31:37 +0600

    just test

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -18,6 +18,8 @@
             <div class="mt-10 flex items-center gap-x-6">
               <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Download CV <i class="fa-regular fa-download"></i></a>
               <!-- <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a> -->
+              <!-- social media icons -->
+              <!-- <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Download CV <i class="fa-regular fa-download"></i></a> -->
             </div>
           </div>
         </div>
