fb06a58215cc91667809b6b272dc053a3f63d313
developer-tamim <tawsiftamim24@gmail.com>
2024-07-05 23:08:13 +0600

    pages setup

---
+++ src/components/pages/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is contact page
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Contact"
+        
+    }
+</script>
+
---
+++ src/components/pages/Services.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        this is services page
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
